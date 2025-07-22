c99e7950d2994ce2dc6037efc3097bd590383e7b
christinanikolova <christinanikolova@github.com>
2025-07-22 07:00:55 +0300

    cleanup

---
+++ server/middlewares/guards.ts
@@ -0,0 +1,44 @@
+import { Response, NextFunction } from "express";
+import { AuthenticatedRequest } from "../interfaces/AuthRequest";
+import global from "../utils/constants/global";
+import parser from "../utils/parser";
+
+const { emails, errors } = global;
+const { mapErrors } = parser;
+
+function isAdmin() {
+  return (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    if (req.user && req.user.email === emails.ADMIN) {
+      next();
+    } else {
+      const message = mapErrors({ message: errors.NOT_LOGGED_IN });
+      res.status(401).json({ message });
+    }
+  };
+}
+
+function hasUser() {
+  return (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    if (req.user) {
+      next();
+    } else {
+      res.status(401).json({ message: errors.NOT_LOGGED_IN });
+    }
+  };
+}
+
+function isGuest() {
+  return (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    if (req.user) {
+      res.status(400).json({ message: errors.ALREADY_LOGGED_IN });
+    } else {
+      next();
+    }
+  };
+}
+
+export default {
+  isAdmin,
+  hasUser,
+  isGuest,
+};
