b4021bac18e7dbb2bf535293a3c358d6abc79fda
christinanikolova <christinanikolova@github.com>
2025-07-22 07:35:43 +0300

    add session middleware

--- server/config/express.ts
+++ server/config/express.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "../middlewares/cors";
+import session from "../middlewares/session";
 
 export default (app: express.Application) => {
+  app.use(express.json());
   app.use(cors());
+  app.use(session());
 };
---
+++ server/middlewares/session.ts
@@ -0,0 +1,30 @@
+import { Response, NextFunction } from "express";
+import authService from "../services/auth";
+import global from "../utils/constants/global";
+import { AuthRequest } from "../interfaces/AuthRequest";
+
+const { errors } = global;
+const { parseToken } = authService;
+
+const session =
+  () => async (req: AuthRequest, res: Response, next: NextFunction) => {
+    const authHeader = req.headers["x-authorization"];
+    const token =
+      authHeader && typeof authHeader === "string"
+        ? authHeader.split(" ")[1]
+        : undefined;
+
+    if (token) {
+      try {
+        const payload = await parseToken(token);
+        req.user = payload;
+        req.token = token;
+      } catch (err) {
+        return res.status(401).json({ message: errors.TOKEN_INVALID });
+      }
+    }
+
+    next();
+  };
+
+export default session;
