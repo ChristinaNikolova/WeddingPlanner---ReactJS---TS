1e77cdaaea92822d127bc831a01a557239c3af52
christinanikolova <christinanikolova@github.com>
2025-07-22 07:00:24 +0300

    cleanup

--- server/utils/mapper/subtask.ts
+++
@@ -1,9 +0,0 @@
-function subtaskViewModel(subtask) {
-  return {
-    id: subtask._id,
-    description: subtask.description,
-    isDone: subtask.isDone,
-  };
-}
-
-export default subtaskViewModel;
