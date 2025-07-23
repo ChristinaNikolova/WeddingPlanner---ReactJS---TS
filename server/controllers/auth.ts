6d5a63612a04632719b2238250b8caf468ad23d7
christinanikolova <christinanikolova@github.com>
2025-07-23 07:07:33 +0300

    add auth controller

--- server/controllers/auth.ts
+++ server/controllers/auth.ts
@@ -1,26 +1,92 @@
 import { Request, Response, Router } from "express";
+import { body, validationResult } from "express-validator";
+import auth from "../services/auth";
+import guards from "../middlewares/guards";
+import global from "../utils/constants/global";
+import parser from "../utils/parser";
+import model from "../utils/constants/model";
 
 const router = Router();
+const { register, login, logout } = auth;
+const { isGuest, hasUser } = guards;
+const { errors: globalErrors } = global;
+const { mapErrors } = parser;
+const { user } = model;
 
-router.post("/register", (req: Request, res: Response) => {
-  console.log("Register route called!");
-  res.status(200).json({
-    success: true,
-    message: "Register endpoint works!",
-  });
-});
+router.post(
+  "/register",
+  isGuest(),
+  body("email").isEmail().withMessage(globalErrors.INVALID_EMAIL),
+  body("password")
+    .isLength({ min: user.PASSWORD_MIN_LEN })
+    .withMessage(
+      globalErrors.PASSWORD(user.PASSWORD_MIN_LEN, user.PASSWORD_MAX_LEN)
+    ),
+  body("password")
+    .isLength({ max: user.PASSWORD_MAX_LEN })
+    .withMessage(
+      globalErrors.PASSWORD(user.PASSWORD_MIN_LEN, user.PASSWORD_MAX_LEN)
+    ),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const result = validationResult(req);
 
-router.post("/login", (req: Request, res: Response) => {
-  console.log("Login route called!");
-  res.status(200).json({
-    success: true,
-    message: "Login endpoint works!",
-  });
-});
+      if (!result.isEmpty()) {
+        const errors = result.array();
+        throw mapErrors(errors);
+      }
 
-router.get("/test", (req: Request, res: Response) => {
-  console.log("TEST route called!");
-  res.json({ message: "Test route works!" });
-});
+      const token = await register(
+        req.body.firstName,
+        req.body.lastName,
+        req.body.email,
+        req.body.password
+      );
+      res.json(token);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
+
+router.post(
+  "/login",
+  isGuest(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const token = await login(req.body.email, req.body.password);
+      res.json(token);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(401).json({ message });
+    }
+  }
+);
+
+router.get(
+  "/logout",
+  hasUser(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const authHeader = req.headers["x-authorization"];
+      if (!authHeader || typeof authHeader !== "string") {
+        // тодо адд еррорс ин глобал
+        throw new Error("Authorization header missing");
+      }
+
+      const token = authHeader.split(" ")[1];
+      if (!token) {
+        throw new Error("Invalid authorization format");
+      }
+
+      await logout(token);
+      res.status(204).end();
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
 
 export default router;
