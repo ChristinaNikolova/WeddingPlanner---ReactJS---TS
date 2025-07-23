4f5d17a3ad7874be41eaebe449014641600bf60b
christinanikolova <christinanikolova@github.com>
2025-07-23 10:43:01 +0300

    update trim middleware

--- server/middlewares/trimBody.ts
+++ server/middlewares/trimBody.ts
@@ -1,11 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 
 const trimBody = () => (req: Request, res: Response, next: NextFunction) => {
-  Object.keys(req.body).forEach((key) => {
-    if (typeof req.body[key] === "string") {
-      req.body[key] = req.body[key].trim();
-    }
-  });
+  if (req.body) {
+    Object.keys(req.body).forEach((key) => {
+      if (typeof req.body[key] === "string") {
+        req.body[key] = req.body[key].trim();
+      }
+    });
+  }
 
   next();
 };
