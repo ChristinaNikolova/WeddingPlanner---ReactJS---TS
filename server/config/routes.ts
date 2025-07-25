f107ed219ba16ba9c0e2ccaffb14407a6d0745b6
christinanikolova <christinanikolova@github.com>
2025-07-25 07:27:24 +0300

    add + test empty constrollers

--- server/config/routes.ts
+++ server/config/routes.ts
@@ -3,6 +3,14 @@ import homeRoutes from "../controllers/home";
 import authRoutes from "../controllers/auth";
 import categoriesRoutes from "../controllers/categories";
 import articlesRoutes from "../controllers/articles";
+import costsRoutes from "../controllers/costs";
+import eventsRoutes from "../controllers/events";
+import guestsRoutes from "../controllers/guests";
+import notesRoutes from "../controllers/notes";
+import plannersRoutes from "../controllers/planners";
+import subtasksRoutes from "../controllers/subtasks";
+import tasksRoutes from "../controllers/tasks";
+import usersRoutes from "../controllers/users";
 import adminArticlesController from "../controllers/admin/articles";
 import adminCategoriesController from "../controllers/admin/categories";
 
@@ -11,6 +19,14 @@ export default function routesConfig(app: express.Application): void {
   app.use("/auth", authRoutes);
   app.use("/categories", categoriesRoutes);
   app.use("/articles", articlesRoutes);
+  app.use("/costs", costsRoutes);
+  app.use("/events", eventsRoutes);
+  app.use("/guests", guestsRoutes);
+  app.use("/notes", notesRoutes);
+  app.use("/planners", plannersRoutes);
+  app.use("/subtasks", subtasksRoutes);
+  app.use("/tasks", tasksRoutes);
+  app.use("/users", usersRoutes);
   app.use("/admin/articles", adminArticlesController);
   app.use("/admin/categories", adminCategoriesController);
 }
---
+++ server/controllers/tasks.ts
@@ -0,0 +1,11 @@
+import { Request, Response, Router } from "express";
+import global from "../utils/constants/global";
+
+const router = Router();
+const { messages } = global;
+
+router.get("/", (req: Request, res: Response): void => {
+  res.json({ message: "Tasks" });
+});
+
+export default router;
