e2fe9105622ec1f44dd2f269ace3568049b26314
christinanikolova <christinanikolova@github.com>
2025-07-25 11:23:42 +0300

    add costs controller

--- server/controllers/costs.ts
+++ server/controllers/costs.ts
@@ -1,11 +1,91 @@
 import { Request, Response, Router } from "express";
-import global from "../utils/constants/global";
+import costs from "../services/costs";
+import guards from "../middlewares/guards";
+import parser from "../utils/parser";
 
 const router = Router();
-const { messages } = global;
+const { all, create, deleteById, update, getById } = costs;
+const { hasUser } = guards;
+const { mapErrors } = parser;
 
-router.get("/", (req: Request, res: Response): void => {
-  res.json({ message: "Costs" });
-});
+router.get(
+  "/:id",
+  hasUser(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const plannerId = req.params.id;
+      const costs = await all(plannerId);
+      res.json(costs);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
+
+router.post(
+  "/:id",
+  hasUser(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const plannerId = req.params.id;
+      const cost = await create(
+        plannerId,
+        req.body.title,
+        req.body.price,
+        req.body.category
+      );
+      res.json(cost);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
+
+router.delete(
+  "/:id",
+  hasUser(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const id = req.params.id;
+      const cost = await deleteById(id);
+      res.json(cost);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  hasUser(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const id = req.params.id;
+      const cost = await update(id, req.body.title, req.body.price);
+      res.json(cost);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
+
+router.get(
+  "/:plannerId/:costId",
+  hasUser(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const id = req.params.costId;
+      const cost = await getById(id, true);
+      res.json(cost);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
 
 export default router;
