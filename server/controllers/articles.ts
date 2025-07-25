9ae987ad8f84988c486e4633285f3650c6070832
christinanikolova <christinanikolova@github.com>
2025-07-25 06:39:42 +0300

    finish articles controller

--- server/controllers/articles.ts
+++ server/controllers/articles.ts
@@ -1,13 +1,14 @@
 import { Request, Response, Router } from "express";
+import { AuthRequest } from "../interfaces/AuthRequest";
 import articles from "../services/articles";
 import guards from "../middlewares/guards";
 import global from "../utils/constants/global";
 import parser from "../utils/parser";
 
 const router = Router();
-const { getLastThree, getById } = articles;
+const { getLastThree, getById, like, getTotalCount, all } = articles;
 const { hasUser } = guards;
-const { messages } = global;
+const { pagination } = global;
 const { mapErrors } = parser;
 
 router.get("/", async (req: Request, res: Response): Promise<void> => {
@@ -20,29 +21,39 @@ router.get("/", async (req: Request, res: Response): Promise<void> => {
   }
 });
 
-// router.get("/:page/:category/:query?", async (req, res) => {
-//   try {
-//     const currentPage = req.params.page;
-//     const selectedCategory =
-//       req.params.category !== "default" ? req.params.category : "";
-//     const searchedQuery = req.params.query || "";
+router.get(
+  "/:page/:category",
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      // todo интерфейс
+      // todo test category + query + pagination
+      const currentPage = Number(req.params.page);
+      const selectedCategory =
+        req.params.category !== "default" ? req.params.category : "";
+      const searchedQuery = (req.query.query as string) || "";
 
-//     const skip = (currentPage - 1) * pagination.ARTICLES_PER_PAGE;
-//     const totalArticles = await getTotalCount(selectedCategory, searchedQuery);
-//     const pagesCount = Math.ceil(totalArticles / pagination.ARTICLES_PER_PAGE);
+      const skip = (currentPage - 1) * pagination.ARTICLES_PER_PAGE;
+      const totalArticles = await getTotalCount(
+        selectedCategory,
+        searchedQuery
+      );
+      const pagesCount = Math.ceil(
+        totalArticles / pagination.ARTICLES_PER_PAGE
+      );
 
-//     const articles = await all(
-//       pagination.ARTICLES_PER_PAGE,
-//       skip,
-//       selectedCategory,
-//       searchedQuery
-//     );
-//     res.json({ articles, pagesCount, currentPage });
-//   } catch (error) {
-//     const message = mapErrors(error);
-//     res.status(400).json({ message });
-//   }
-// });
+      const articles = await all(
+        pagination.ARTICLES_PER_PAGE,
+        skip,
+        selectedCategory,
+        searchedQuery
+      );
+      res.json({ articles, pagesCount, currentPage });
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
 
 router.get(
   "/:id",
@@ -59,17 +70,21 @@ router.get(
   }
 );
 
-// router.post("/:id", hasUser(), async (req, res) => {
-//   try {
-//     const id = req.params.id;
-//     const userId = req.user._id;
+router.post(
+  "/:id",
+  hasUser(),
+  async (req: AuthRequest, res: Response): Promise<void> => {
+    try {
+      const id = req.params.id;
+      const userId = req.user._id;
 
-//     const article = await like(id, userId);
-//     res.json(article);
-//   } catch (error) {
-//     const message = mapErrors(error);
-//     res.status(400).json({ message });
-//   }
-// });
+      const article = await like(id, userId);
+      res.json(article);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
 
 export default router;
