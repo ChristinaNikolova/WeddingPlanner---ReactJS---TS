9287634a95afeef94e91f3e068c54c646f696a0a
christinanikolova <christinanikolova@github.com>
2025-07-25 22:27:47 +0300

    add notes controller

--- server/controllers/notes.ts
+++ server/controllers/notes.ts
@@ -1,11 +1,86 @@
 import { Request, Response, Router } from "express";
-import global from "../utils/constants/global";
+import notes from "../services/notes";
+import guards from "../middlewares/guards";
+import parser from "../utils/parser";
 
 const router = Router();
-const { messages } = global;
+const { all, create, deleteById, update, getById } = notes;
+const { hasUser } = guards;
+const { mapErrors } = parser;
 
-router.get("/", (req: Request, res: Response): void => {
-  res.json({ message: "Notes" });
-});
+router.get(
+  "/:id",
+  hasUser(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const plannerId = req.params.id;
+      const notes = await all(plannerId);
+      res.json(notes);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
+
+router.post(
+  "/:id",
+  hasUser(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const plannerId = req.params.id;
+      const note = await create(plannerId, req.body.description);
+      res.json(note);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
+
+router.delete(
+  "/:id",
+  hasUser(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const id = req.params.id;
+      const note = await deleteById(id);
+      res.json(note);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  hasUser(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const id = req.params.id;
+      const note = await update(id, req.body.description);
+      res.json(note);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
+
+router.get(
+  "/:plannerId/:noteId",
+  hasUser(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const id = req.params.noteId;
+      const note = await getById(id, true);
+      res.json(note);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
 
 export default router;
