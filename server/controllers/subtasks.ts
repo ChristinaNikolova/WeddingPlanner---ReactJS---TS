4efae64929c71a058e056178e222f488aa002aab
christinanikolova <christinanikolova@github.com>
2025-07-27 07:58:42 +0300

    add subtasks controller

--- server/controllers/subtasks.ts
+++ server/controllers/subtasks.ts
@@ -1,11 +1,90 @@
 import { Request, Response, Router } from "express";
-import global from "../utils/constants/global";
+import guards from "../middlewares/guards";
+import subtasks from "../services/subtasks";
+import parser from "../utils/parser";
 
 const router = Router();
-const { messages } = global;
+const { hasUser } = guards;
+const { create, done, deleteById, update, getById } = subtasks;
+const { mapErrors } = parser;
 
-router.get("/", (req: Request, res: Response): void => {
-  res.json({ message: "Subtasks" });
-});
+router.post(
+  "/:id",
+  hasUser(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const taskId = req.params.id;
+      const subtask = await create(taskId, req.body.description);
+      res.json(subtask);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
+
+router.post(
+  "/:taskId/:subtaskId",
+  hasUser(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const taskId = req.params.taskId;
+      const subtaskId = req.params.subtaskId;
+
+      const subtask = await done(taskId, subtaskId);
+      res.json(subtask);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
+
+router.delete(
+  "/:taskId/:subtaskId",
+  hasUser(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const taskId = req.params.taskId;
+      const subtaskId = req.params.subtaskId;
+
+      const subtask = await deleteById(taskId, subtaskId);
+      res.json(subtask);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  hasUser(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const id = req.params.id;
+      const subtask = await update(id, req.body.description);
+      res.json(subtask);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
+
+router.get(
+  "/:id",
+  hasUser(),
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const id = req.params.id;
+      const subtask = await getById(id, true);
+      res.json(subtask);
+    } catch (error) {
+      const message = mapErrors(error);
+      res.status(400).json({ message });
+    }
+  }
+);
 
 export default router;
