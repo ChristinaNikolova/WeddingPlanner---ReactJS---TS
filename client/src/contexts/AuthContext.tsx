194410d3ec778c518dc2a6e372d819465d6e8b89
christinanikolova <christinanikolova@github.com>
2025-07-29 07:24:38 +0300

    finish authcontext

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -17,14 +17,15 @@ interface AuthProviderProps {
   children: ReactNode;
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const AuthContext = createContext<AuthContextValue | undefined>(
   undefined
 );
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  const [authToken, setAuthToken] = useSessionStorage("authToken", "");
-  const [email, setEmail] = useSessionStorage("email", "");
-  const [id, setId] = useSessionStorage("userId", "");
+  const [authToken, setAuthToken] = useSessionStorage<string>("authToken", "");
+  const [email, setEmail] = useSessionStorage<string>("email", "");
+  const [id, setId] = useSessionStorage<string>("userId", "");
 
   const userLogin = (authData: AuthResponse): void => {
     setAuthToken(authData.accessToken);
