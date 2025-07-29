fda3a2fcad5cc47d81f6d43a83c7aa863d1b2bd0
christinanikolova <christinanikolova@github.com>
2025-07-29 07:34:08 +0300

    add logout component

---
+++ client/src/components/Auth/Logout/Logout.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthContext";
+import * as authService from "../../../services/auth";
+
+function Logout() {
+  const { userLogout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    authService
+      .logout()
+      .then(() => {
+        userLogout();
+        navigate("/");
+      })
+      .catch((err) => console.error(err));
+  }, []);
+}
+
+export default Logout;
