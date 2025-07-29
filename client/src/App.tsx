759f18362abcc13097a2687c7a898f7e372b6697
christinanikolova <christinanikolova@github.com>
2025-07-29 17:12:01 +0300

    add Footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthProvider";
 // import * as constants from "./utils/constants/images";
 
-// import Footer from "./components/Footer/Footer";
+import Footer from "./components/Footer/Footer";
 // import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import NotFound from "./components/NotFound/NotFound";
@@ -63,6 +63,7 @@ function App() {
 
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </AuthProvider>
   );
 }
