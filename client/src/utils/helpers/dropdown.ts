89ebfc5ea59a79293ea32e261e78d40c440b218e
christinanikolova <christinanikolova@github.com>
2025-07-29 09:22:42 +0300

    add dropdown helpers

---
+++ client/src/utils/helpers/dropdown.ts
@@ -0,0 +1,29 @@
+import { classNames, displayStyles } from "../constants/global";
+
+export const toggle = (
+  element: HTMLElement,
+  remove: string,
+  add: string
+): void => {
+  element.classList.remove(remove);
+  element.classList.add(add);
+};
+
+export const toggleWithTargetContent = (
+  targetElement: HTMLElement,
+  targetIcon: HTMLElement
+): void => {
+  const isHidden = targetElement.style.display === displayStyles.NONE;
+
+  targetElement.style.display = isHidden
+    ? (targetElement.style.display = displayStyles.BLOCK)
+    : (targetElement.style.display = displayStyles.NONE);
+
+  if (targetIcon.classList.contains(classNames.CHEVRON_DOWN)) {
+    targetIcon.classList.remove(classNames.CHEVRON_DOWN);
+    targetIcon.classList.add(classNames.CHEVRON_RIGHT);
+  } else {
+    targetIcon.classList.remove(classNames.CHEVRON_RIGHT);
+    targetIcon.classList.add(classNames.CHEVRON_DOWN);
+  }
+};
