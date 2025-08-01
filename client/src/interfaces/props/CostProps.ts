9c839402d3247237568196e2dd5c56aa415ad06f
christinanikolova <christinanikolova@github.com>
2025-08-01 06:34:44 +0300

    add costs service

---
+++ client/src/interfaces/props/CostProps.ts
@@ -0,0 +1,7 @@
+export interface CostProps {
+  id: string;
+  title: string;
+  price: string;
+  category: string;
+}
+// todo rename?
---
+++ client/src/services/costs.ts
@@ -0,0 +1,87 @@
+import type { ArticleProps } from "../interfaces/props/articles/ArticleProps";
+import type { CostProps } from "../interfaces/props/CostProps";
+import { httpMethods, serviceNames } from "../utils/constants/global";
+import { handleServiceError } from "../utils/helpers/errorHandler";
+import { api } from "./api";
+import { requester } from "./requester";
+
+export const all = async (plannerId: string): Promise<CostProps> => {
+  try {
+    const response = await requester(
+      `${api.public.costs}/${plannerId}`,
+      httpMethods.GET
+    );
+    return response.json();
+  } catch (error) {
+    return handleServiceError(error, serviceNames.COSTS);
+  }
+};
+
+export const create = async (
+  plannerId: string,
+  title: string,
+  price: string,
+  category: string
+): Promise<ArticleProps> => {
+  try {
+    const response = await requester(
+      `${api.public.costs}/${plannerId}`,
+      httpMethods.POST,
+      {
+        title,
+        price,
+        category,
+      }
+    );
+    return response.json();
+  } catch (error) {
+    return handleServiceError(error, serviceNames.COSTS);
+  }
+};
+
+export const deleteById = async (id: string): Promise<CostProps> => {
+  try {
+    const response = await requester(
+      `${api.public.costs}/${id}`,
+      httpMethods.DELETE
+    );
+    return response.json();
+  } catch (error) {
+    return handleServiceError(error, serviceNames.COSTS);
+  }
+};
+
+export const getById = async (
+  plannerId: string,
+  costId: string
+): Promise<CostProps> => {
+  try {
+    const response = await requester(
+      `${api.public.costs}/${plannerId}/${costId}`,
+      httpMethods.GET
+    );
+    return response.json();
+  } catch (error) {
+    return handleServiceError(error, serviceNames.COSTS);
+  }
+};
+
+export const update = async (
+  id: string,
+  title: string,
+  price: string
+): Promise<CostProps> => {
+  try {
+    const response = await requester(
+      `${api.public.costs}/${id}`,
+      httpMethods.PUT,
+      {
+        title,
+        price,
+      }
+    );
+    return response.json();
+  } catch (error) {
+    return handleServiceError(error, serviceNames.COSTS);
+  }
+};
