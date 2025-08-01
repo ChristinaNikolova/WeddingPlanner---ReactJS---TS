4ad21437a9902f76fbeb97a9eec2896e2efd8d80
christinanikolova <christinanikolova@github.com>
2025-08-01 06:58:45 +0300

    add guests service

---
+++ client/src/interfaces/props/GuestProps.ts
@@ -0,0 +1,14 @@
+export interface GuestProps {
+  id: string;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  age: string;
+  side: string;
+  role: string;
+  table: string;
+  mainDish: string;
+  confirmed: boolean;
+}
+
+// todo check why the article returns objectId
---
+++ client/src/services/guests.ts
@@ -0,0 +1,114 @@
+import { api } from "./api";
+import { requester } from "./requester";
+import type { GuestProps } from "../interfaces/props/GuestProps";
+import { httpMethods, serviceNames } from "../utils/constants/global";
+import { handleServiceError } from "../utils/helpers/errorHandler";
+
+export const all = async (plannerId: string): Promise<GuestProps[]> => {
+  try {
+    const response = await requester(
+      `${api.public.guests}/${plannerId}`,
+      httpMethods.GET
+    );
+    return response.json();
+  } catch (error) {
+    return handleServiceError(error, serviceNames.GUESTS);
+  }
+};
+
+// todo use the types here??
+export const create = async (
+  plannerId: string,
+  firstName: string,
+  lastName: string,
+  gender: string,
+  age: string,
+  side: string,
+  role: string,
+  table: string,
+  mainDish: string,
+  confirmed: boolean
+): Promise<GuestProps> => {
+  try {
+    const response = await requester(
+      `${api.public.guests}/${plannerId}`,
+      httpMethods.POST,
+      {
+        firstName,
+        lastName,
+        gender,
+        age,
+        side,
+        role,
+        table,
+        mainDish,
+        confirmed,
+      }
+    );
+    return response.json();
+  } catch (error) {
+    return handleServiceError(error, serviceNames.GUESTS);
+  }
+};
+
+export const deleteById = async (id: string): Promise<GuestProps> => {
+  try {
+    const response = await requester(
+      `${api.public.guests}/${id}`,
+      httpMethods.DELETE
+    );
+    return response.json();
+  } catch (error) {
+    return handleServiceError(error, serviceNames.GUESTS);
+  }
+};
+
+// todo use types here??
+export const update = async (
+  id: string,
+  firstName: string,
+  lastName: string,
+  gender: string,
+  age: string,
+  side: string,
+  role: string,
+  table: string,
+  mainDish: string,
+  confirmed: boolean
+): Promise<GuestProps> => {
+  try {
+    const response = await requester(
+      `${api.public.guests}/${id}`,
+      httpMethods.PUT,
+      {
+        firstName,
+        lastName,
+        gender,
+        age,
+        side,
+        role,
+        table,
+        mainDish,
+        confirmed,
+      }
+    );
+    return response.json();
+  } catch (error) {
+    return handleServiceError(error, serviceNames.GUESTS);
+  }
+};
+
+export const getById = async (
+  plannerId: string,
+  guestId: string
+): Promise<GuestProps> => {
+  try {
+    const response = await requester(
+      `${api.public.guests}/${plannerId}/${guestId}`,
+      httpMethods.GET
+    );
+    return response.json();
+  } catch (error) {
+    return handleServiceError(error, serviceNames.GUESTS);
+  }
+};
