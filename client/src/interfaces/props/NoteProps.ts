786fdb9374908f17f395171106475dbab709af3a
christinanikolova <christinanikolova@github.com>
2025-08-01 07:30:04 +0300

    add css for notes

--- client/src/interfaces/props/NoteProps.ts
+++ client/src/interfaces/props/NoteProps.ts
@@ -1,5 +1,8 @@
+import type { ErrorProps } from "./shared/Errors/ErrorProps";
+
 export interface NoteProps {
   id: string;
   description: string;
   createdAt: string;
+  message?: ErrorProps[];
 }
