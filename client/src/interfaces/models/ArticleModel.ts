9642bc2718aabf89a5d5b1ef7ffb540e51715020
christinanikolova <christinanikolova@github.com>
2025-08-06 07:58:19 +0300

    update interfaces

--- client/src/interfaces/models/ArticleModel.ts
+++ client/src/interfaces/models/ArticleModel.ts
@@ -2,7 +2,7 @@ import type { CategoryProps } from "../props/categories/CategoryProps";
 
 export interface ArticleModel {
   title: string;
-  content: string;
+  content: string | string[];
   image: string;
   jumboImage: string;
   category: CategoryProps | string;
--- client/src/interfaces/props/articles/ArticleDetailsProps.ts
+++ client/src/interfaces/props/articles/ArticleDetailsProps.ts
@@ -1,12 +1,8 @@
-import type { CategoryProps } from "../categories/CategoryProps";
 import type { ArticleProps } from "./ArticleProps";
 
 export interface ArticleDetailsProps extends ArticleProps {
-  jumboImage: string;
-  content: string[];
   likesCount: number;
   likes: string[];
-  category: CategoryProps;
   categoryName: string;
   selectedCategory?: string;
 }
